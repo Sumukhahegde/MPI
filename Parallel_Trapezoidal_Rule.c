df590db84ca690626894897fe74ab13fe13cc1a0
Suhas Jain Suresh <suhas.jain2000@gmail.com>
2015-10-30 20:33:38 +0530

    Now input is working with broadcast option

--- Parallel_Trapezoidal_Rule.c
+++ Parallel_Trapezoidal_Rule.c
@@ -2,7 +2,18 @@
 #include <math.h>
 #include "mpi.h"
 
+void Get_data1(double *a_ptr, double *b_ptr, long long int *n_ptr, int my_rank){
 
+	if(my_rank == 0){
+		printf("Enter a, b and n\n");
+		scanf("%lf %lf %lld", a_ptr, b_ptr, n_ptr);
+	}
+
+	MPI_Bcast(a_ptr, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD); 
+	MPI_Bcast(b_ptr, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD); 
+	MPI_Bcast(n_ptr, 1, MPI_LONG, 0, MPI_COMM_WORLD);
+
+} 
 
 void Get_data(double *a_ptr, double *b_ptr, long long int *n_ptr, int my_rank, int p){
 
@@ -19,9 +30,10 @@ void Get_data(double *a_ptr, double *b_ptr, long long int *n_ptr, int my_rank, i
 			tag = 0;
 			MPI_Send(a_ptr, 1, MPI_DOUBLE, dest, tag, MPI_COMM_WORLD);
 			tag = 1;
+	printf("I am process number %d", p);
 			MPI_Send(b_ptr, 1, MPI_DOUBLE, dest, tag, MPI_COMM_WORLD);
 			tag = 2;
-			MPI_Send(n_ptr, 1, MPI_DOUBLE, dest, tag, MPI_COMM_WORLD);
+			MPI_Send(n_ptr, 1, MPI_LONG, dest, tag, MPI_COMM_WORLD);
 		}
 	} else{
 		tag = 0;
@@ -29,7 +41,7 @@ void Get_data(double *a_ptr, double *b_ptr, long long int *n_ptr, int my_rank, i
 		tag = 1;
 		MPI_Recv(b_ptr, 1, MPI_DOUBLE, source, tag, MPI_COMM_WORLD, &status);
 		tag = 2;
-		MPI_Recv(n_ptr, 1, MPI_DOUBLE, source, tag, MPI_COMM_WORLD, &status);
+		MPI_Recv(n_ptr, 1, MPI_LONG, source, tag, MPI_COMM_WORLD, &status);
 	}
 
 }
@@ -66,9 +78,9 @@ main(int argc, char** argv){
 
 	int p;
 	int my_rank;
-	double a;
-	double b;
-	long long int n;
+	double a = -2.0;
+	double b = 2.0;
+	long long int n = 1000;
 	double h;	//base lenght of each trapezoid
 	double local_a;
 	double local_b;
@@ -79,15 +91,19 @@ main(int argc, char** argv){
 	int dest = 0;
 	int tag = 0;
 	MPI_Status status;
+//	printf("I am process number %d", p);
 	
 	MPI_Init(&argc, &argv);
+//	printf("I am process number %d", p);
 
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
 	MPI_Comm_size(MPI_COMM_WORLD, &p);
 
+	printf("I am process number %d\n", my_rank);
 
-	Get_data(&a, &b, &n, my_rank, p); 
+	Get_data1(&a, &b, &n, my_rank);
+//	Get_data(&a, &b, &n, my_rank, p); 
 
 
 	h = ( b - a ) / n;
